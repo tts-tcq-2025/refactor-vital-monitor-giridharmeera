9509f27bc6474977fa1f58aa7a645f594ebb959a
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-03 00:03:02 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -2,7 +2,6 @@
 #include <string>
 #include <functional>
 
-// Struct representing a vital sign check
 struct VitalCheck {
     std::string name;
     float value;
@@ -10,8 +9,7 @@ struct VitalCheck {
     float max;
 };
 
-// Check one vital sign
 bool checkVital(const VitalCheck& vital, std::function<void(const std::string&)> alert);
 
-// Check all vitals
-int vitalsOk(float temperature, float pulseRate, float spo2, std::function<void(const std::string&)> alert = nullptr);
+int vitalsOk(float temperature, float pulseRate, float spo2,
+             std::function<void(const std::string&)> alert = nullptr);
