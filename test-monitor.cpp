ad77ec4ca6746d2cc44767882127ac1c980fc6d1
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-03 00:03:37 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,46 +1,37 @@
-#include <gtest/gtest.h>
 #include "./monitor.h"
+#include <gtest/gtest.h>
 #include <vector>
 #include <string>
 
-TEST(Monitor, AllVitalsOk) 
-{
-    ASSERT_TRUE(vitalsOk(98.6, 70, 95));
-}
-
-TEST(Monitor, TemperatureOutOfRangeHigh) 
-{
+TEST(Monitor, TemperatureOutOfRangeHigh) {
     ASSERT_FALSE(vitalsOk(103.0, 70, 95));
 }
 
-TEST(Monitor, TemperatureOutOfRangeLow) 
-{
+TEST(Monitor, TemperatureOutOfRangeLow) {
     ASSERT_FALSE(vitalsOk(94.0, 70, 95));
 }
 
-TEST(Monitor, PulseRateOutOfRangeHigh) 
-{
+TEST(Monitor, PulseRateOutOfRangeHigh) {
     ASSERT_FALSE(vitalsOk(98.6, 105.0, 95));
 }
 
-TEST(Monitor, PulseRateOutOfRangeLow) 
-{
+TEST(Monitor, PulseRateOutOfRangeLow) {
     ASSERT_FALSE(vitalsOk(98.6, 50.0, 95));
 }
 
-TEST(Monitor, Spo2OutOfRangeLow) 
-{
+TEST(Monitor, Spo2OutOfRangeLow) {
     ASSERT_FALSE(vitalsOk(98.6, 70, 85.0));
 }
 
-TEST(Monitor, CustomAlertCapturesMessages) 
-{
+TEST(Monitor, AllVitalsOk) {
+    ASSERT_TRUE(vitalsOk(98.6, 70, 95));
+}
+
+TEST(Monitor, CustomAlertCapturesMessages) {
     std::vector<std::string> messages;
-    auto captureAlert = [&messages](const std::string& msg) 
-    {
+    auto captureAlert = [&messages](const std::string& msg) {
         messages.push_back(msg);
     };
-
     ASSERT_FALSE(vitalsOk(98.6, 105.0, 95, captureAlert));
     ASSERT_EQ(messages.size(), 1);
     ASSERT_EQ(messages[0], "Pulse Rate is out of range!");
