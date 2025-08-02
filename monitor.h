bdd6b5358944ed07af192f6032e67776550c4f8e
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-02 23:50:41 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,17 @@
 #pragma once
+#include <string>
+#include <functional>
 
-int vitalsOk(float temperature, float pulseRate, float spo2);
+// Struct representing a vital sign check
+struct VitalCheck {
+    std::string name;
+    float value;
+    float min;
+    float max;
+};
+
+// Check one vital sign
+bool checkVital(const VitalCheck& vital, std::function<void(const std::string&)> alert);
+
+// Check all vitals
+int vitalsOk(float temperature, float pulseRate, float spo2, std::function<void(const std::string&)> alert = nullptr);
