e508ff28fcd81c169f31933ec2fb0cf8661a203e
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-02 23:48:19 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -5,9 +5,12 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-void AlertMessage(const char* message) {
+// Default alert: console output with blinking stars
+void defaultAlert(const std::string& message) 
+{
     cout << message << "\n";
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 6; ++i) 
+    {
         cout << "\r* " << flush;
         sleep_for(seconds(1));
         cout << "\r *" << flush;
@@ -15,18 +18,39 @@ void AlertMessage(const char* message) {
     }
 }
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-    if (temperature > 102 || temperature < 95) {
-        AlertMessage("Temperature is critical!");
-        return 0;
+// Check one vital
+bool checkVital(const VitalCheck& vital, std::function<void(const std::string&)> alert) 
+{
+    if (vital.value < vital.min || vital.value > vital.max) 
+    {
+        alert(vital.name + " is out of range!");
+        return false;
     }
-    if (pulseRate < 60 || pulseRate > 100) {
-        AlertMessage("Pulse Rate is out of range!");
-        return 0;
+    return true;
+}
+
+// Main function to check all vitals 
+int vitalsOk(float temperature, float pulseRate, float spo2, std::function<void(const std::string&)> alert) 
+{
+    if (!alert) 
+    {
+        alert = defaultAlert;
     }
-    if (spo2 < 90) {
-        AlertMessage("Oxygen Saturation out of range!");
-        return 0;
+
+    VitalCheck vitals[] = 
+    {
+        {"Temperature", temperature, 95.0, 102.0},
+        {"Pulse Rate", pulseRate, 60.0, 100.0},
+        {"Oxygen Saturation", spo2, 90.0, 100.0}
+    };
+
+    for (int i = 0; i < 3; ++i) 
+    {
+        if (!checkVital(vitals[i], alert)) 
+        {
+            return 0;
+        }
     }
+
     return 1;
 }
