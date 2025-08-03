21265375860188daeda63419ca88f18010e747b7
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-03 11:04:46 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,39 +3,41 @@
 #include <string>
 #include "./monitor.hpp"
 
+std::vector<std::string> messages;
+auto captureAlert = [&messages](const std::string& msg) {
+        messages.push_back(msg);
+    };
+
 TEST(Monitor, TemperatureOutOfRangeHigh) {
-    ASSERT_FALSE(vitalsOk(103.0, 70, 95));
+    ASSERT_FALSE(vitalsOk(103.0, 70, 95,captureAlert));
 }
 
 TEST(Monitor, TemperatureOutOfRangeLow) {
-    ASSERT_FALSE(vitalsOk(94.0, 70, 95));
+    ASSERT_FALSE(vitalsOk(94.0, 70, 95,captureAlert));
 }
 
 TEST(Monitor, PulseRateOutOfRangeHigh) {
-    ASSERT_FALSE(vitalsOk(98.6, 105.0, 95));
+    ASSERT_FALSE(vitalsOk(98.6, 105.0, 95,captureAlert));
 }
 
 TEST(Monitor, PulseRateOutOfRangeLow) {
-    ASSERT_FALSE(vitalsOk(98.6, 50.0, 95));
+    ASSERT_FALSE(vitalsOk(98.6, 50.0, 95,captureAlert));
 }
 
 TEST(Monitor, Spo2OutOfRangeLow) {
-    ASSERT_FALSE(vitalsOk(98.6, 70, 85.0));
+    ASSERT_FALSE(vitalsOk(98.6, 70, 85.0,captureAlert));
 }
 
 TEST(Monitor, AllVitalsOk) {
-    ASSERT_TRUE(vitalsOk(98.6, 70, 95));
+    ASSERT_TRUE(vitalsOk(98.6, 70, 95,captureAlert));
 }
 
 TEST(Monitor, CustomAlertCapturesMessages) {
-    std::vector<std::string> messages;
-    auto captureAlert = [&messages](const std::string& msg) {
-        messages.push_back(msg);
-    };
     ASSERT_FALSE(vitalsOk(98.6, 105.0, 95, captureAlert));
     ASSERT_EQ(messages.size(), 1);
     ASSERT_EQ(messages[0], "Pulse Rate is out of range!");
 }
+
 
 
 
