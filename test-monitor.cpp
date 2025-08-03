3c45d8e1601b153602eb92d66f75f1828ec02b0b
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-03 10:47:51 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -27,5 +27,16 @@ TEST(Monitor, AllVitalsOk) {
     ASSERT_TRUE(vitalsOk(98.6, 70, 95));
 }
 
+TEST(Monitor, CustomAlertCapturesMessages) {
+    std::vector<std::string> messages;
+    auto captureAlert = [&messages](const std::string& msg) {
+        messages.push_back(msg);
+    };
+    ASSERT_FALSE(vitalsOk(98.6, 105.0, 95, captureAlert));
+    ASSERT_EQ(messages.size(), 1);
+    ASSERT_EQ(messages[0], "Pulse Rate is out of range!");
+}
+
+
 
 
