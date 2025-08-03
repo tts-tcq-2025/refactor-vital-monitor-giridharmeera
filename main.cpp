ff0a1410c581f8f7b91b8aebb413bae256ffecc1
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-03 11:38:00 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <chrono>
 #include "./monitor.hpp"
+
+void PrintAlertMessage(const std::string& message) {
+    cout << message << "\n";
+    for (int i = 0; i < 6; ++i) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+}
 
 int main() {
     vitalsOk(103.0, 70, 95,PrintAlertMessage);
