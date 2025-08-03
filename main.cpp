dd390b532075e9ac878a6cb8d0392fcc980d74cd
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-03 11:41:19 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <chrono>
 #include "./monitor.hpp"
 
+using std::cout, std::flush;
+using std::this_thread::sleep_for;
+using std::chrono::seconds;
+
 void PrintAlertMessage(const std::string& message) {
     cout << message << "\n";
     for (int i = 0; i < 6; ++i) {
