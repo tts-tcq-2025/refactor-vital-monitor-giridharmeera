effee0ee84af855b1a5add387164ff403a5d5569
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-03 11:11:01 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -34,9 +34,9 @@ TEST(Monitor, AllVitalsOk) {
 
 TEST(Monitor, CustomAlertCapturesMessages) {
     ASSERT_FALSE(vitalsOk(98.6, 105.0, 95, captureAlert));
-    ASSERT_EQ(messages.size(), 1);
-    ASSERT_EQ(messages[0], "Pulse Rate is out of range!");
+    ASSERT_EQ(messages[6], "Pulse Rate is out of range!");
 }
+
 
 
 
