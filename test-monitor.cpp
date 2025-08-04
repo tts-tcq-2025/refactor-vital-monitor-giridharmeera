61f273ed326dbb99a069714e5b138fb4273e4299
Meera Giridhar <giridharmeera1994@gmail.com>
2025-08-04 18:53:44 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -9,32 +9,33 @@ auto captureAlert = [&messages](const std::string& msg) {
     };
 
 TEST(Monitor, TemperatureOutOfRangeHigh) {
-    ASSERT_FALSE(vitalsOk(103.0, 70, 95,captureAlert));
+    ASSERT_FALSE(vitalsOk(103.0, 70, 95, captureAlert));
 }
 
 TEST(Monitor, TemperatureOutOfRangeLow) {
-    ASSERT_FALSE(vitalsOk(94.0, 70, 95,captureAlert));
+    ASSERT_FALSE(vitalsOk(94.0, 70, 95, captureAlert));
 }
 
 TEST(Monitor, PulseRateOutOfRangeHigh) {
-    ASSERT_FALSE(vitalsOk(98.6, 105.0, 95,captureAlert));
+    ASSERT_FALSE(vitalsOk(98.6, 105.0, 95, captureAlert));
 }
 
 TEST(Monitor, PulseRateOutOfRangeLow) {
-    ASSERT_FALSE(vitalsOk(98.6, 50.0, 95,captureAlert));
+    ASSERT_FALSE(vitalsOk(98.6, 50.0, 95, captureAlert));
 }
 
 TEST(Monitor, Spo2OutOfRangeLow) {
-    ASSERT_FALSE(vitalsOk(98.6, 70, 85.0,captureAlert));
+    ASSERT_FALSE(vitalsOk(98.6, 70, 85.0, captureAlert));
 }
 
 TEST(Monitor, AllVitalsOk) {
-    ASSERT_TRUE(vitalsOk(98.6, 70, 95,captureAlert));
+    ASSERT_TRUE(vitalsOk(98.6, 70, 95, captureAlert));
 }
 
 TEST(Monitor, CustomAlertCapturesMessages) {
     ASSERT_FALSE(vitalsOk(98.6, 105.0, 95, captureAlert));
 }
+
 
 
 
